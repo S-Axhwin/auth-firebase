4146849411f8e03b874e6c7bb504a7748d8bb9fa
S-Axhwin <ashwin.sathiyamoorthy.d@gmail.com>
2024-06-05 02:45:13 +0530

    fixed bug

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -2,7 +2,7 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
 
-const page =  () => {
+const Page =  () => {
     const [post, setPost] = useState(null) as any;
     useEffect(() => {
       (async() => {
@@ -23,4 +23,4 @@ const page =  () => {
   )
 }
 
-export default page
+export default Page
